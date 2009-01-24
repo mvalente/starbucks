a57175701b1f02407731c36d392886a4b9639d45
Mario Valente <mfvalente@gmail.com>
2009-01-24 12:39:38 -0500

    Barebones webserver doing GET+POST

---
+++ cimbali/cimbali.js
@@ -0,0 +1,168 @@
+#!/usr/local/bin/jshost
+
+
+LoadModule('jsstd');
+LoadModule('jsio');
+
+
+var configuration = { port:8080, address:'0.0.0.0', docroot:'/var/www/db' };
+
+var descriptorList = [];
+
+
+
+main();
+
+function main()
+{
+   var port = configuration.port;
+   var address = configuration.address;
+   var httpPort = null;
+
+
+
+   try
+   {
+     // Open server port
+     httpPort = new Socket();
+     httpPort.Bind( port, address);
+  
+     descriptorList.push(httpPort);
+
+
+     // Setup callback to deal with incoming connections
+     httpPort.readable = function() 
+          {
+  		var httpSocket = httpPort.Accept();
+  
+  		descriptorList.push(httpSocket);
+
+
+  		// Setup callback to deal with incoming data
+  		httpSocket.readable = function(s)
+  					{
+					Print("On socket: "+s.sockName+","+s.sockPort + "\n");
+					Print("On socket: "+s.peerName+","+s.peerPort + "\n");
+  					var data = s.Read();
+
+					Print(data);
+  					processHTTPConnectionRequest(data,s);
+
+					// delete s.readable;
+     					CloseConnection(s);
+
+  					}
+  	}
+
+
+
+     Print( "Waiting on port " + port + ".\n" ); 
+     httpPort.Listen();
+
+
+     // Process HTTP service requests in an infinite loop.
+     while( !endSignal )
+     {
+
+       Poll(descriptorList, 500);
+
+     }
+
+     CloseConnection(httpPort);
+     Print( "END" );
+   }
+   catch( e ) 
+   {
+     throw(e);
+   }
+}
+
+function CloseConnection(s) 
+{
+        s.Close();
+        descriptorList.splice( descriptorList.indexOf(s), 1 );
+}
+
+
+function processHTTPConnectionRequest(data,s)
+{
+	
+	Print('\n\n NEW REQUEST\n');
+	Print(data);
+
+	[ headers, body ] = data.split('\r\n\r\n');
+	// Print(headers+'\n\n');
+
+	headerlines = headers.split('\r\n');
+
+ 	[ method, resource, protocol] = headerlines[0].split(' ');
+	Print('METHOD: ' + method + '\n');
+	Print('RESOURCE: ' + resource + '\n');
+	Print('PROTOCOL: ' + protocol + '\n');
+
+
+        switch (method) 
+	{
+	case 'GET':
+		ProcessGet( resource, s);
+		break;
+	case 'POST':
+		ProcessPost( resource, body, s);
+		break;
+	}
+	Print('---------------------' + '\n\n\n');
+}
+
+function ProcessGet(resource,s)
+{
+        var docroot=configuration.docroot;
+
+        var file = new File( docroot + resource );
+        if ( !file.exist || file.info.type != File.FILE_FILE ) 
+	{
+		Print('File not found');
+		s.Write("HTTP/1.1 404 Not Found\n\n<html><body>404 error</body></html>");
+		return;
+        }
+	file.Open( File.RDONLY );
+	s.TransmitFile(file, false, "HTTP/1.1 200 OK\n\n")
+	file.Sync();
+	file.Close();
+
+}
+
+function ProcessPost(resource,body,s)
+{
+	Print('AT RESOURCE: '+resource+'\n');
+	Print('SAVE DATA:\n');
+	Print(body);
+	Print('\nEND DATA\n\n');
+
+        var docroot=configuration.docroot;
+
+        var file = new File( docroot + resource );
+        file.Open( File.RDWR | File.CREATE_FILE );
+	file.Write(body);
+        file.Sync();
+        file.Close();
+
+        file.Open( File.RDONLY );
+	s.TransmitFile(file, false, "HTTP/1.1 200 OK\nX-ID: 123456789\n\n")
+        file.Sync();
+        file.Close();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
